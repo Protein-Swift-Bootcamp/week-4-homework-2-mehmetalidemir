9401ebf5c3e85a125c37a3710fbb9abcbc728241
Mehmet Ali Demir <64283995+mehmetalidemir@users.noreply.github.com>
2023-01-06 17:29:26 +0300

    Add comment line

--- musicApp/week-4-homework-2-mehmetalidemir/DetailViewController.swift
+++ musicApp/week-4-homework-2-mehmetalidemir/DetailViewController.swift
@@ -14,36 +14,46 @@ class DetailViewController: UIViewController {
 
     @IBOutlet weak var attackTypeLabel: UILabel!
     @IBOutlet weak var heroImage: UIImageView!
-                       
-                       
+
+    // Property to hold the hero data
     var hero: HeroStats?
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Set the label text to the hero's name
         heroLabel.text = hero?.localized_name
+        // Set the attack type label text to the hero's attack type
         attackTypeLabel.text = "Attack Type: \(String(describing: hero?.attack_type ?? ""))"
         let imgUrl = "https://api.opendota.com" + (hero?.img)!
         print(imgUrl)
+        // Download and display the hero image
         heroImage.downloaded(from: imgUrl)
     }
 }
 
+// MARK: - Image Downloading Extension
+
 extension UIImageView {
+    // Function to download and display an image from a URL
     func downloaded(from url: URL, contentMode mode: ContentMode = .scaleAspectFit) {
         contentMode = mode
         URLSession.shared.dataTask(with: url) { data, response, error in
+            // Check that the response is successful and the data is an image
             guard
                 let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
                 let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
                 let data = data, error == nil,
                 let image = UIImage(data: data)
                 else { return }
+            // Update the image on the main queue
             DispatchQueue.main.async() { [weak self] in
                 self?.image = image
             }
         }.resume()
     }
+    // Function to download and display an image from a string URL
     func downloaded(from link: String, contentMode mode: ContentMode = .scaleAspectFit) {
         guard let url = URL(string: link) else { return }
+        // Download and display the image
         downloaded(from: url, contentMode: mode)
     }
 }
--- musicApp/week-4-homework-2-mehmetalidemir/ViewController.swift
+++ musicApp/week-4-homework-2-mehmetalidemir/ViewController.swift
@@ -11,13 +11,13 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-    
+
+    // Array to hold the hero stats data
     var heroes = [HeroStats]()
-
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Fetch herostats data and reload the table view when finished
         fetchData {
             self.tableView.reloadData()
             self.activityIndicator.stopAnimating()
@@ -27,21 +27,27 @@ class ViewController: UIViewController {
 
         tableView.dataSource = self
         tableView.delegate = self
+        // Register the custom cell class for the table view
         tableView.register(UINib.init(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
+        // Show the activity indicator while the data is being fetched
         activityIndicator.startAnimating()
     }
 
 }
 
-// MARK -
+// MARK: - TableView Data Source and Delegate
+
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of heroes in the heroes array
         return heroes.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? TableViewCell
+        // Get the hero for the current row
         let hero = heroes[indexPath.row]
+        // Set the label text to the hero's name
         cell?.lbl.text = hero.localized_name.capitalized
 
         return cell!
@@ -49,15 +55,17 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Get an instance of the DetailViewController and set the hero property to the selected hero
         let vc = storyboard?.instantiateViewController(withIdentifier: "detail") as? DetailViewController
         let hero = heroes[indexPath.row]
         vc?.hero = hero
+        // Set the modal presentation style and present the view controller
         vc?.modalPresentationStyle = .formSheet
         present(vc!, animated: true, completion: nil)
     }
 
 
-
+    // MARK: - Data Fetching
     func fetchData(completed: @escaping () -> ()) {
         let url = URL(string: "https://api.opendota.com/api/heroStats")
 
